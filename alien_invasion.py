26d52fabe92bea96ecf4fb0929851bd510d58ed3
leahnp613 <leahnagarpowers@gmail.com>
2024-02-08 12:58:52 -0500

    docker & docker yaml

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,17 +5,17 @@ from settings import Settings
 from ship import Ship
 
 class AlienInvasion:
- def __init__(self):
-    # Initialize the game, and create game resources.
-    pygame.init()
-    self.clock = pygame.time.Clock()
-    self.settings = Settings()
+    def __init__(self):
+        # Initialize the game, and create game resources.
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-    self.screen = pygame.display.set_mode(
-        (self.settings.screen_width, self.settings.screen_height))
-    pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
 
-    self.ship = Ship(self)
+        self.ship = Ship(self)
     
 
     def run_game(self):
@@ -65,7 +65,7 @@ class AlienInvasion:
 
 
             
-if __name__ == '_main_':
+if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
