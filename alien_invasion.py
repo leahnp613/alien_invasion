9773393d9151f8a44e6ecccbcd5839b012765e75
leahnp613 <leahnagarpowers@gmail.com>
2024-02-13 12:35:08 -0500

    moving left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,9 +36,14 @@ class AlienInvasion:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
                          self.ship.moving_right = True
+                    elif event.key == pygame.L_LEFT:
+                         self.ship.moving_left = True
+
                 elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False          
+                        self.ship.moving_right = False  
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False        
                          #Move the ship to the right.
                         self.ship.rect.x += 1
 
